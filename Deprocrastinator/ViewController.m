9e9468001d5be9c2a2853e6e8dcfb6523ace5006
Amir Bahadori <pbahadori156@gmail.com>
2016-05-31 15:48:20 -0500

    AttributedStrings set

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -11,7 +11,8 @@
 @interface ViewController () <UITableViewDataSource, UITableViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
-@property NSMutableArray *toDoItems;
+@property NSMutableArray <NSMutableAttributedString *> *toDoItems;
+@property (weak, nonatomic) IBOutlet UITextField *taskTextField;
 
 @end
 
@@ -20,19 +21,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.toDoItems = [NSMutableArray arrayWithObjects: @"Take the dog out", @"Finish my afternoon challenge", @"Drink Tequila", @"Call Mom", nil];
+    self.toDoItems = [NSMutableArray arrayWithObjects:
+                      [[NSMutableAttributedString alloc] initWithString: @"Take the dog out"],
+                      [[NSMutableAttributedString alloc] initWithString: @"Finish my afternoon challenge"],
+                      [[NSMutableAttributedString alloc] initWithString: @"Drink Tequila"],
+                      [[NSMutableAttributedString alloc] initWithString: @"Call Mom"], nil];
 
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CellID"];
-    cell.textLabel.text = [NSString stringWithFormat:@"%@", [self.toDoItems objectAtIndex:indexPath.row]];
+    
+    // cell.textLabel.text = [NSString stringWithFormat:@"%@", [self.toDoItems objectAtIndex:indexPath.row]];
+    
+    cell.textLabel.attributedText = [self.toDoItems objectAtIndex:indexPath.row];
     
     return cell;
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return self.toDoItems.count;
+}
+
+- (IBAction)addButtonPressed:(UIBarButtonItem *)sender {
+    // [self.toDoItems addObject:[NSString stringWithFormat:@"%@",self.taskTextField.text]];
+    NSMutableAttributedString *textFieldText = [[NSMutableAttributedString alloc] initWithString: self.taskTextField.text];
+    [self.toDoItems addObject: textFieldText];
+    [self.tableView reloadData];
+    [self.taskTextField resignFirstResponder];
+    
+    // Clears the task after you add a task - really sets the textfield text to nothing
+    self.taskTextField.text = @"";
+    
 }
 
 @end
